ab84f303f85d3341eb12cabf2f2c791cdf7d7bd8
elParadigm <abderraouf.talbi@isimg.tn>
2025-05-04 16:26:10 +0100

    test

---
+++ src/main/java/gui/connexionGui.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.*;
+
+public class connexionGui {
+    private JPanel panel1;
+    private JTextField nomTextField;
+    private JTextField textField2;
+    private JButton seConnecterButton;
+    private JButton pasUnCompteSButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
