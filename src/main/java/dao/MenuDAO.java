3d5bc4e21d1bab81b631251d66010b04ec40925c
elParadigm <abderraouf.talbi@isimg.tn>
2025-05-07 20:03:17 +0100

    test

--- src/main/java/dao/MenuDAO.java
+++ src/main/java/dao/MenuDAO.java
@@ -7,7 +7,7 @@ public class MenuDAO {
     public List<Menu> getMenuList() throws ClassNotFoundException, SQLException {
         List<Menu> menuList = new ArrayList<>();
         String sql = "select * from menu";
-
+//aaaaaaaaahhhhhh menek ya sof
         try (Connection conn = SingletonConnection.getInstance();
              PreparedStatement ps = conn.prepareStatement(sql);
              ResultSet rs = ps.executeQuery()) {
@@ -24,7 +24,7 @@ public class MenuDAO {
     public void ajouterMenu(Menu menu) throws ClassNotFoundException, SQLException {
         String sql = "insert into menu (nom,description) values (?,?)";
         try (Connection conn = SingletonConnection.getInstance();
-        PreparedStatement ps = conn.prepareStatement(sql)) {
+             PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, menu.getNomMenu());
             ps.setString(2, menu.getDescriptionMenu());
             ps.executeUpdate();
@@ -32,9 +32,10 @@ public class MenuDAO {
 
     }
 
+    public void miseAJourMenu(@org.jetbrains.annotations.NotNull Menu menu) throws ClassNotFoundException, SQLException {
         String sql = "Update menu set nom=?, description=? where idMenu=?";
         try (Connection conn = SingletonConnection.getInstance();
-        PreparedStatement ps = conn.prepareStatement(sql)) {
+             PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, menu.getNomMenu());
             ps.setString(2, menu.getDescriptionMenu());
             ps.setInt(3, menu.getIdMenu());
@@ -45,10 +46,9 @@ public class MenuDAO {
     public void suprimerMenu(int menuID) throws ClassNotFoundException, SQLException {
         String sql = "delete from menu where idMenu=?";
         try (Connection conn = SingletonConnection.getInstance();
-        PreparedStatement ps = conn.prepareStatement(sql)) {
+             PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setInt(1, menuID);
             ps.executeUpdate();
         }
     }
 }
-
