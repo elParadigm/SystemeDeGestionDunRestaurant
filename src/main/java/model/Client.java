475035c962699a346ffb963c34c600372398dfa8
Soufyan Rachdi <soyfsoyf20004@gmail.com>
2025-05-07 20:45:57 +0100

    zedt constructor par defaut

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -4,4 +4,7 @@ public class Client extends Utilisateur{
     public Client(int idUtilisateur, String nomUtilisateur, String mdpUtilisateur) {
         super(idUtilisateur,nomUtilisateur,mdpUtilisateur,"Client");
     }
+    public Client(){
+        super();
+    }
 }
