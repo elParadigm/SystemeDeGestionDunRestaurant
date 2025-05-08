122fb8a09815c48924ba893a33f8f4712f846171
Soufyan Rachdi <soyfsoyf20004@gmail.com>
2025-05-08 17:02:28 +0100

    pour executer le projet a le page login

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import gui.Login;
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Pour s'assurer que l'interface graphique s'exécute sur le bon thread
+        SwingUtilities.invokeLater(() -> {
+            try {
+                // Lance la fenêtre de login ET la rend visible
+                Login login = new Login();
+                login.setVisible(true); // ← C'est cette ligne qui manquait
+                System.out.println("jjj");
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erreur lors du lancement de l'interface : " + e.getMessage());
+                e.printStackTrace();
+            }
+        });
     }
 }
