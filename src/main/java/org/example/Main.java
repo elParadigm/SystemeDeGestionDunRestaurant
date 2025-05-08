7d8ebee53cef79c7b20428d0747616dac7793886
Soufyan Rachdi <soyfsoyf20004@gmail.com>
2025-05-08 19:24:58 +0100

    testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,21 @@
 package org.example;
 
+import dao.SingletonConnection;
 import gui.Login;
 import javax.swing.*;
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            Connection connection = SingletonConnection.getInstance();
+            if (connection != null) {
+                JOptionPane.showMessageDialog(null, "Connexion réussie à la base de données !");
+                System.out.println("Connexion réussie !");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         // Pour s'assurer que l'interface graphique s'exécute sur le bon thread
         SwingUtilities.invokeLater(() -> {
             try {
